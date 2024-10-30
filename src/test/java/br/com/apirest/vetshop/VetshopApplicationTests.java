cc563f6e8de899fd6c23e76b1ecaf39954163c55
Lennon <lennonmartins@digix.com.br>
2024-10-29 22:15:36 -0400

    Feat: subindo a primeira versão

---
+++ src/test/java/br/com/apirest/vetshop/VetshopApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.apirest.vetshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VetshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
